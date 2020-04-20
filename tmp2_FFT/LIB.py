95758d26b14be20e624285d3f84a3bd4c46fd9d1
Serena1999 <bruzzesi.serena@gmail.com>
2020-04-20 21:13:41 +0200

    singola_sinusoidale

---
+++ tmp2_FFT/LIB.py
@@ -0,0 +1,20 @@
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                           LIBRARIES                                 #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+import pylab
+import numpy as np
+numpy = np
+import math
+from scipy.optimize import curve_fit
+import scipy
+from statistics import *
+import os # operazioni su file
+import shutil # operazioni su file
+from statistics import *
+from matplotlib import pyplot as plt
+from scipy.stats import norm
+import time
+
+##nuova aggiunta
+#import cmath
